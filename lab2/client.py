49d1dc4503e1287cb4ebfac63180a3ad055767e2
hiufungk <hiufungk@ualberta.ca>
2019-01-17 19:32:05 -0500

    changed client.py to follow lab

--- lab2/client.py
+++ lab2/client.py
@@ -2,17 +2,38 @@
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-address = ('www.google.com', 80)
-sock.connect(address)
-request = b"GET / HTTP/1.0\n\n"
-sock.sendall(request)
+HOST = "www.google.com"
+PORT = 80
+BUFFER_SIZE = 1024
 
-all_result = b""
-result = sock.recv(10000)
-all_result += result
-while (len(result) > 0):
-    #print(result)
-    result = sock.recv(10000)
-    all_result += result
-print(all_result)
+payload = "GET / HTTP/1.0\r\nHost: {}\r\n\r\n".format(HOST)
+
+def get_request(addr):
+    (family, socktype, proto, cannonname, sockaddr) = addr
+    try:
+        s = socket.socket(family, socktype, proto)
+        s.connect(sockaddr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
+        
+        full_data = b""
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            if not data: break
+            full_data += data
+        print(full_data)
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+def main():
+    addr_info = socket.getaddrinfo(HOST, PORT)
+    for addr in addr_info:
+        (family, socktype, proto, cannonname, sockaddr) = addr
+        if family == socket.AF_INET and socktype == socket.SOCK_STREAM:
+            #print(addr)
+            get_request(addr)
+
+if __name__ == "__main__":
+    main()
